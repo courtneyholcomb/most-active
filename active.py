43066e1325176f1370c9a16fb0e9708131b3a941
Courtney Holcomb <courtneyeholcomb@gmail.com>
2019-09-17 13:35:42 -0700

    completed challenge

--- active.py
+++ active.py
@@ -35,6 +35,46 @@ Since there's a tie, the first was returned)
 def most_active(bio_data):
     """Find window of time when most authors were active."""
 
+    # if multiple, are they adjacent/do they overlap?
+    # if yes, take full range. if no, take first range.
+
+    date_set = set()
+    for info in bio_data:
+        date_set.update(info[1:])
+    dates = sorted(list(date_set))
+
+    active = {}
+    for info in bio_data:
+        for i in range(len(dates) - 1):
+            span = (dates[i], dates[i + 1])
+            # if span start and end between author start and end
+            # author start <== span start; author end >= span end
+            if info[1] <= span[0] and info[2] >= span[1]:
+                active[span] = active.get(span, []) + [info[0]]
+
+    max_len = 0
+    max_spans = None
+    for span, authors in active.items():
+        if len(authors) > max_len:
+            max_len = len(authors)
+            max_spans = [span]
+        elif len(authors) == max_len:
+            max_spans += [span]
+
+    if len(max_spans) > 1:
+        start_date = max_spans[0][0]
+        end_date = max_spans[0][1]
+        for i in range(len(max_spans) - 1):
+            if end_date >= max_spans[i + 1][0]:
+                end_date = max_spans[i + 1][1]
+    else:
+        return max_spans[0]
+
+    return (start_date, end_date)
+
+
+
+
 if __name__ == '__main__':
     import doctest
     if doctest.testmod().failed == 0:
